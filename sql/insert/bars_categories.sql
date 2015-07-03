cf47f0733169487a3aa321e9e47dd6300f0ce531
marcohern <marcohern@gmail.com>
2015-07-03 16:59:34 -0500

    added new cover column

--- sql/insert/bars_categories.sql
+++ sql/insert/bars_categories.sql
@@ -2,6 +2,7 @@ INSERT INTO bars_categories(id, slug, name) VALUES
 (1, 'bar','Bar'),
 (2, 'disco','Discoteca'),
 (3, 'restaurant','Restaurante'),
-(4, 'xxx','Xxx'),
-(5, 'disco-gay','Discoteca Gay'),
+-- (4, 'xxx','Xxx'),
+-- (5, 'disco-gay','Discoteca Gay'),
 (6, 'casino','Casino');
+
