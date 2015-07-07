f6706a3d151ac1db7689189d408c4813a1507a1f
marcohern <marcohern@gmail.com>
2015-07-07 11:32:26 -0500

    added genres

---
+++ sql/insert/bars_genres.sql
@@ -0,0 +1,17 @@
+INSERT INTO bars_genres(id, name, slug) VALUES
+(1,'Rock','rock'),
+(2,'Pop','pop'),
+(3,'Rap','rap'),
+(4,'Ska','ska'),
+(5,'Reggae','reggae'),
+(6,'Blues','blues'),
+(7,'Jazz','jazz'),
+(8,'Lounge','lounge'),
+(9,'Clasica','clasica'),
+(10,'Reggaeton','reggaeton'),
+(11,'Salsa','salsa'),
+(12,'Cumbia','cumbia'),
+(13,'Electronica','electronica'),
+(14,'Crossover','crossover'),
+(15,'Bailables','bailables');
+
